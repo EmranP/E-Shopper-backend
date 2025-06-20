1898a5a16e23acd35aa5101ccf26c9191f5a5769
Emran <nightsking12345@gmail.com>
2025-06-21 01:14:29 +0500

    Add pagination for get product all

--- src/controllers/product/getProducts.controller.ts
+++ src/controllers/product/getProducts.controller.ts
@@ -4,7 +4,9 @@ import logger from '../../utils/logger.utils'
 
 export const getProducts: RequestHandler = async (req, res, next) => {
 	try {
-		const products = await productService.getProducts()
+		const {limit = 10, offset = 0} = req.query
+
+		const products = await productService.getProducts(Number(limit), Number(offset))
 
 		logger.info('Продукты успешно получены из controller')
 		res.status(200).json(products)
